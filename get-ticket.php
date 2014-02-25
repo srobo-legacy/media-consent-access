46539238693471c8c2ba3fc09ba01ed24a7abe95
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-25 13:37:14 -0800

    Deliver an internal error message

--- get-ticket.php
+++ get-ticket.php
@@ -12,6 +12,7 @@ if (!file_exists($ticket_name)) {
     exec("cd pdfs ; python generate.py $user -y 2013 -d 'April 13th-14th (Doors open 9:00)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
     if (!file_exists($ticket_name)) {
         header('HTTP/1.1 500 Internal Server Error');
+        print "Internal error -- please contact info@studentrobotics.org";
         exit();
     }
 }
