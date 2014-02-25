369c4efe96754e0b90e29b2821bcf853a1b307d8
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-25 13:54:45 -0800

    Correct pdf output path.

--- get-ticket.php
+++ get-ticket.php
@@ -1,15 +1,16 @@
 <?php
 
 $user = $_SERVER['REMOTE_USER'];
+$user = "ide";
 
-$base_dir = dirname(__FILE__) . '/tickets/pdfs';
+$base_dir = dirname(__FILE__) . '/pdfs';
 
 $ticket_name = "$base_dir/$user.pdf";
 
 if (!file_exists($ticket_name)) {
     # run the generation script
 
-    exec("cd pdfs ; python generate.py $user -y 2013 -d 'April 13th-14th (Doors open 9:00)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
+    exec("cd tickets; python generate.py $user -y 2013 -d 'April 13th-14th (Doors open 9:00)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
     if (!file_exists($ticket_name)) {
         header('HTTP/1.1 500 Internal Server Error');
         print "Internal error -- please contact info@studentrobotics.org";
