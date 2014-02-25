3f5cfb754a90f60bb881597c3e18499d7c28028f
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-25 13:22:15 -0800

    Mush MCF server to not deliver a failure page.
    Instead, just deliver a 500 internal error. If the user authenticated,
    then we should always be able to serve them, there's no scope for
    failure, aside from internal failure.

--- get-ticket.php
+++ get-ticket.php
@@ -2,25 +2,21 @@
 
 $user = $_SERVER['REMOTE_USER'];
 
-$base_dir = dirname(__FILE__) . '/tickets/webapi/users';
+$base_dir = dirname(__FILE__) . '/tickets/pdfs';
 
 $ticket_name = "$base_dir/$user.pdf";
 
 if (!file_exists($ticket_name)) {
     # run the generation script
 
-    exec("cd tickets ; python generate.py $user -y 2013 -d 'April 13th-14th (Doors open 9:00)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
+    exec("cd pdfs ; python generate.py $user -y 2013 -d 'April 13th-14th (Doors open 9:00)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
     if (!file_exists($ticket_name)) {
-        header('HTTP/1.1 403 Forbidden');
-        header('Content-type: text/html');
-        header('Content-length: ' . filesize('nope.html'));
-        readfile('nope.html');
+        header('HTTP/1.1 500 Internal Server Error');
         exit();
     }
 }
 
 header('Content-type: application/pdf');
 header('Content-length: ' . filesize($ticket_name));
-header('Content-Disposition: attachment; filename="ticket.pdf"');
+header('Content-Disposition: attachment; filename="mediaconsent.pdf"');
 readfile($ticket_name);
-
