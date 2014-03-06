41a9a11003891536e075b107ecd279d382100eb4
Peter Law <PeterJCLaw@gmail.com>
2014-03-06 20:27:03 +0000

    Make the use of inputs in an exec slightly safer.
    Change-Id: I588ae57f0f4de25ded7791a314503b3dbcbd9c40

--- get-mcf.php
+++ get-mcf.php
@@ -9,7 +9,9 @@ $ticket_name = "$base_dir/$user.pdf";
 if (!file_exists($ticket_name)) {
     # run the generation script
 
-    exec("cd tickets; python generate.py $user -o $ticket_name 2>&1", $output, $rv);
+    $s_user = escapeshellarg($user);
+    $s_ticket_name = escapeshellarg($ticket_name);
+    exec("cd tickets; python generate.py $s_user -o $s_ticket_name 2>&1", $output, $rv);
     if (!file_exists($ticket_name)) {
         header('HTTP/1.1 500 Internal Server Error');
         print "Internal error -- please contact info@studentrobotics.org";
