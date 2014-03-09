ed635473cadf9f4ee9178dc44a850aea440d5a70
Peter Law <PeterJCLaw@gmail.com>
2014-03-09 00:07:08 +0000

    Prevent withdrawn users getting media consent forms.
    They're not attending, so emitting these forms makes no sense.
    Add a nicer error mesage for the specific error code it raises.
    
    2014-03-09 Patch set 2: Bump submodule for typo fix.
    
    Change-Id: I7041274c3ebef7029e2d697a2a32546df9d8e116

--- get-mcf.php
+++ get-mcf.php
@@ -12,6 +12,12 @@ if (!file_exists($ticket_name)) {
     $s_user = escapeshellarg($user);
     $s_ticket_name = escapeshellarg($ticket_name);
     exec("cd tickets; python generate.py $s_user -o $s_ticket_name 2>&1", $output, $rv);
+    if ($rv == 3) {
+        header('HTTP/1.1 403 Forbidden');
+        print "This user account has been withdrawn, and may not attend the competition.<br />";
+        print "If you believe this account should not be withdrawn, please contact your team-leader.";
+        exit();
+    }
     if (!file_exists($ticket_name)) {
         header('HTTP/1.1 500 Internal Server Error');
         print "Internal error -- please contact info@studentrobotics.org";
